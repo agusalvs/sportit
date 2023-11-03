0bd31be95a1454e2e72977ba93171d30c3c60efa
agusalvs <agustina.alvarezsacra@gmail.com>
2023-11-03 11:02:25 -0300

    Made changes on mobile and desktop versions to show/not show the burger menu and the way it shows on the screen. Also had the js file modified so that the burger menu has an Event Listener on click. Still needs some changes to be done

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-// Obtenemos referencias a los elementos del menú
-const burger = document.getElementById('burger');
-const menuList = document.getElementById('menu-list');
+// Espera a que el documento esté completamente cargado
+document.addEventListener('DOMContentLoaded', function() {
+  // Obtenemos referencias a los elementos del menú
+  const burger = document.getElementById('burger');
+  const menuList = document.getElementById('menu-list');
 
-// Agregamos un evento de clic al menú hamburguesa
-burger.addEventListener('click', () => {
-  // Alternamos la visibilidad de la lista desplegable
-  if (menuList.style.display === 'block') {
-    menuList.style.display = 'none';
-  } else {
-    menuList.style.display = 'block';
-  }
+  // Agregamos un evento de clic al menú hamburguesa
+  burger.addEventListener('click', () => {
+      // Alternamos la visibilidad de la lista desplegable
+      if (menuList.style.display === 'block') {
+          menuList.style.display = 'none';
+      } else {
+          menuList.style.display = 'block';
+      }
+  });
 });
